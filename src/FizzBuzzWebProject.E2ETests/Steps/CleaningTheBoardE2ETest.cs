fa50f293d45b8c0b40b3218a5ee164131a474e52
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-10-04 11:00:02 -0400

    Adding Clean board E2E Test

---
+++ src/FizzBuzzWebProject.E2ETests/Steps/CleaningTheBoardE2ETest.cs
@@ -0,0 +1,25 @@
+﻿using FizzBuzzWebProject.E2ETests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FizzBuzzWebProject.E2ETests.Steps
+{
+    public class CleaningTheBoardE2ETest : TestBase
+    {
+        private FizzBuzzPage fizzBuzzPage;
+        public CleaningTheBoardE2ETest() => fizzBuzzPage = new FizzBuzzPage(driver);
+
+        [Fact]
+        public void CleaningTheBoard_WhenCleanButtonisClicked_NumbersAreErased()
+        {
+            fizzBuzzPage.GotoFizzBuzzPage();
+            fizzBuzzPage.runTestButton.Click();
+
+            fizzBuzzPage.cleanButton.Click();
+
+            Assert.Empty(fizzBuzzPage.fizzBuzzResults);
+        }
+    }
+}
